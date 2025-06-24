7a3d4d4787ba86a5d28aeaf995d1bfbc13a5b32c
SilverTrust123 <SilverTrust123@users.noreply.github.com>
2025-06-24 15:45:16 +0000

    deploy: 54f753aa8190a588d7335a6c441826ad0e036eb6

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "412671394"
+    serviceWorkerVersion: "1793088648"
   }
 });
