0eba9a9fa4c61bcf23b74db860595411b62a5a29
SilverTrust123 <forschool819@gmail.com>
2025-06-26 20:05:04 +0800

    ad  the web controller mouse used util

--- flutter_application_1/lib/src/widgets/forecast_horizontal_widget.dart
+++ flutter_application_1/lib/src/widgets/forecast_horizontal_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/main.dart';
 import 'package:flutter_application_1/src/model/weather.dart';
@@ -5,8 +6,6 @@ import 'package:flutter_application_1/src/widgets/value_tile.dart';
 import 'package:intl/intl.dart';
 
 /// Renders a horizontal scrolling list of weather conditions
-/// Used to show forecast
-/// Shows DateTime, Weather Condition icon and Temperature
 class ForecastHorizontal extends StatelessWidget {
   const ForecastHorizontal({
     Key? key,
@@ -19,30 +18,38 @@ class ForecastHorizontal extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 70,
-      child: ListView.separated(
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        itemCount: this.weathers.length,
-        separatorBuilder: (context, index) => Divider(
-          height: 100,
-          color: Colors.white,
+      child: ScrollConfiguration(
+        behavior: ScrollConfiguration.of(context).copyWith(
+          dragDevices: {
+            PointerDeviceKind.touch,
+            PointerDeviceKind.mouse,
+          },
         ),
-        padding: EdgeInsets.only(left: 10, right: 10),
-        itemBuilder: (context, index) {
-          final item = this.weathers[index];
-          return Padding(
-            padding: const EdgeInsets.only(left: 10, right: 10),
-            child: Center(
-              child: ValueTile(
-                label: DateFormat('E, ha').format(
-                    DateTime.fromMillisecondsSinceEpoch(item.time * 1000)),
-                value:
-                    '${item.temperature.as(AppStateContainer.of(context).temperatureUnit).round()}°',
-                iconData: item.getIconData(),
+        child: ListView.separated(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          itemCount: this.weathers.length,
+          separatorBuilder: (context, index) => Divider(
+            height: 100,
+            color: Colors.white,
+          ),
+          padding: EdgeInsets.only(left: 10, right: 10),
+          itemBuilder: (context, index) {
+            final item = this.weathers[index];
+            return Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Center(
+                child: ValueTile(
+                  label: DateFormat('E, ha').format(
+                      DateTime.fromMillisecondsSinceEpoch(item.time * 1000)),
+                  value:
+                      '${item.temperature.as(AppStateContainer.of(context).temperatureUnit).round()}°',
+                  iconData: item.getIconData(),
+                ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
